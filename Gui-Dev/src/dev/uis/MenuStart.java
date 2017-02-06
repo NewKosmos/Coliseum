72de34a94b4320e7002662490b447c96938d06c6
Matthew Albrecht <mattparks5855@gmail.com>
2017-02-06 10:51:55 -0600

    Better paralax

--- Gui-Dev/src/dev/uis/MenuStart.java
+++ Gui-Dev/src/dev/uis/MenuStart.java
@@ -12,15 +12,22 @@ package dev.uis;
 import flounder.devices.*;
 import flounder.framework.*;
 import flounder.guis.*;
+import flounder.maths.vectors.*;
 import flounder.resources.*;
 import flounder.textures.*;
+import flounder.visual.*;
 
 import java.util.*;
 
 public class MenuStart extends GuiComponent {
+	private static final Vector2f PARALAX_START = new Vector2f(0.5f, 0.5f);
+	private static final float PARALAX_PERIOD = 10.0f;
+
 	private MasterMenu superMenu;
 	private MasterSlider masterSlider;
 
+	private LinearDriver paralaxDriver;
+	private Vector2f paralaxPosition;
 	private GuiTexture t1 = new GuiTexture(Texture.newTexture(new MyFile(MyFile.RES_FOLDER, "1.png")).create());
 	private GuiTexture t2 = new GuiTexture(Texture.newTexture(new MyFile(MyFile.RES_FOLDER, "2.png")).create());
 	private GuiTexture t3 = new GuiTexture(Texture.newTexture(new MyFile(MyFile.RES_FOLDER, "3.png")).create());
@@ -29,6 +36,9 @@ public class MenuStart extends GuiComponent {
 	public MenuStart(MasterMenu superMenu, MasterSlider masterSlider) {
 		this.superMenu = superMenu;
 		this.masterSlider = masterSlider;
+
+		this.paralaxDriver = new LinearDriver(0.0f, 360.0f, PARALAX_PERIOD);
+		this.paralaxPosition = new Vector2f();
 
 		/*float currentY = 1.0f + MasterSlider.BUTTONS_Y_SEPARATION;
 		createQuitButton(currentY -= MasterSlider.BUTTONS_Y_SEPARATION);
@@ -55,34 +65,36 @@ public class MenuStart extends GuiComponent {
 
 	@Override
 	protected void updateSelf() {
+		Vector2f.rotate(PARALAX_START, paralaxDriver.update(FlounderFramework.getDelta()), paralaxPosition);
+
 		float extraWidth = 0.15f;
 		float extraHeight = 0.15f;
 
-		float moveX = FlounderMouse.getPositionX() - 1.0f;
-		float moveY = FlounderMouse.getPositionY() - 1.0f;
+		float moveX = (FlounderMouse.getPositionX() - 1.0f) + paralaxPosition.x;
+		float moveY = (FlounderMouse.getPositionY() - 1.0f) + paralaxPosition.y;
 
 		t1.setPosition(FlounderDisplay.getAspectRatio() / 2.0f, 0.5f, FlounderDisplay.getAspectRatio(), 1.0f);
 		t1.update();
 
 		t2.setPosition(
-				(FlounderDisplay.getAspectRatio() - (extraWidth * -moveX)) / 2.0f,
-				(1.0f - (extraHeight * moveY)) / 2.0f,
+				(FlounderDisplay.getAspectRatio() - (extraWidth * 0.1f * -moveX)) / 2.0f,
+				(1.0f - (extraHeight * 0.1f * moveY)) / 2.0f,
 				FlounderDisplay.getAspectRatio() + extraWidth,
 				1.0f + extraHeight
 		);
 		t2.update();
 
 		t3.setPosition(
-				(FlounderDisplay.getAspectRatio() - (extraWidth * moveX)) / 2.0f,
-				(1.0f - (extraHeight * -moveY)) / 2.0f,
+				(FlounderDisplay.getAspectRatio() - (extraWidth * 0.2f * moveX)) / 2.0f,
+				(1.0f - (extraHeight * 0.2f * -moveY)) / 2.0f,
 				FlounderDisplay.getAspectRatio() + extraWidth,
 				1.0f + extraHeight
 		);
 		t3.update();
 
 		t4.setPosition(
-				(FlounderDisplay.getAspectRatio() - (extraWidth * moveX)) / 2.0f,
-				(1.0f - (extraHeight * moveY)) / 2.0f,
+				(FlounderDisplay.getAspectRatio() - (extraWidth * 0.3f * moveX)) / 2.0f,
+				(1.0f - (extraHeight * 0.3f * moveY)) / 2.0f,
 				FlounderDisplay.getAspectRatio() + extraWidth,
 				1.0f + extraHeight
 		);
