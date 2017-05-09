b6b7266484101c5b5b8f4c5231cd3e54f244e473
Matthew Albrecht <mattparks5855@gmail.com>
2017-05-09 12:18:06 -0500

    Bones!

--- src/kosmos/entities/instances/InstancePlayer.java
+++ src/kosmos/entities/instances/InstancePlayer.java
@@ -19,7 +19,7 @@ public class InstancePlayer extends Entity {
 	public InstancePlayer(ISpatialStructure<Entity> structure, Vector3f position, Vector3f rotation) {
 		super(structure, position, rotation);
 		new ComponentPlayer(this);
-		new ComponentAnimation(this, 0.2f, COLLADA, TEXTURE, 1);
+		new ComponentAnimation(this, 0.12f, COLLADA, TEXTURE, 1);
 		new ComponentSurface(this, 1.0f, 0.0f, false, false, true);
 		//	new flounder.entities.components.ComponentLight(this, new Vector3f(0.0f, 2.0f, 0.0f), new Colour(1.0f, 1.0f, 1.0f), new Attenuation(1.0f, 0.02f, 0.5f));
 		new ComponentCollision(this);
